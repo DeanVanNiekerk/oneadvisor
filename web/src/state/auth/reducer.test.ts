944c8ca74c468c0fc7903867db4422c34cffe4fd
Dean van Niekerk <deanvniekerk@gmail.com>
2018-12-18 21:36:13 +0200

    types fix

--- web/src/state/auth/reducer.test.ts
+++ web/src/state/auth/reducer.test.ts
@@ -1,48 +1,56 @@
 import { defaultState, reducer } from './reducer';
 
 describe('auth reducer', () => {
-
     it('should handle AUTH_RECIEVE_AUTHENTICATION', () => {
-
         const payload = {
-            userInfo: { firstName: 'Dean', lastName: 'Jonny' },
+            userInfo: {
+                given_name: 'Dean',
+                family_name: 'Jonny',
+                email: '',
+                name: '',
+                sub: '1'
+            },
             idToken: '1234134',
-            accessToken: '431212',
-        }
+            accessToken: '431212'
+        };
 
         const actualState = reducer(defaultState, {
-            type: "AUTH_RECIEVE_AUTHENTICATION",
+            type: 'AUTH_RECIEVE_AUTHENTICATION',
             payload: { ...payload }
-        })
+        });
 
         const expectedState = {
             ...defaultState,
             ...payload,
-            authenticated: true,
-        }
+            authenticated: true
+        };
 
-        expect(actualState).toEqual(expectedState)
-    })
+        expect(actualState).toEqual(expectedState);
+    });
 
     it('should handle AUTH_RECIEVE_AUTHENTICATION_CLEAR', () => {
-
         const initalState = {
             ...defaultState,
             authenticated: true,
-            userInfo: { firstName: 'Dean', lastName: 'Jonny' },
+            userInfo: {
+                given_name: 'Dean',
+                family_name: 'Jonny',
+                email: '',
+                name: '',
+                sub: '1'
+            },
             idToken: '1234134',
             accessToken: '431212'
-        }
+        };
 
         const actualState = reducer(initalState, {
-            type: "AUTH_RECIEVE_AUTHENTICATION_CLEAR"
-        })
+            type: 'AUTH_RECIEVE_AUTHENTICATION_CLEAR'
+        });
 
         const expectedState = {
-            ...defaultState,
-        }
+            ...defaultState
+        };
 
-        expect(actualState).toEqual(expectedState)
-    })
-
-})
+        expect(actualState).toEqual(expectedState);
+    });
+});
