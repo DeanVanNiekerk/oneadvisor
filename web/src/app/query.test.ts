7b6bd84f96bbe179adf8849d615af88138c2cd85
Dean van Niekerk <deanvniekerk@gmail.com>
2018-12-18 18:25:06 +0200

    remove lib breaking build

--- web/src/app/query.test.ts
+++ web/src/app/query.test.ts
@@ -2,20 +2,32 @@ import { appendQueryString } from './query';
 
 describe('query', () => {
     it('appendQueryString - no existing query string', () => {
-        const actual = appendQueryString('https://oneadvisor', {
-            param1: 1,
-            param2: '2'
-        });
+        const actual = appendQueryString('https://oneadvisor', [
+            {
+                key: 'param1',
+                value: '1'
+            },
+            {
+                key: 'param2',
+                value: '2'
+            }
+        ]);
 
         expect(actual).toBe('https://oneadvisor?param1=1&param2=2');
     });
 
     it('appendQueryString - has existing query string', () => {
-        const actual = appendQueryString('https://oneadvisor?paramx=yyy', {
-            param1: 1,
-            param2: '2'
-        });
+        const actual = appendQueryString('https://oneadvisor?paramx=yyy', [
+            {
+                key: 'param1',
+                value: '1'
+            },
+            {
+                key: 'param2',
+                value: '2'
+            }
+        ]);
 
-        expect(actual).toBe('https://oneadvisor?param1=1&param2=2&paramx=yyy');
+        expect(actual).toBe('https://oneadvisor?paramx=yyy&param1=1&param2=2');
     });
 });
--- web/src/app/query.ts
+++ web/src/app/query.ts
@@ -1,15 +1,19 @@
-import queryString from 'query-string';
-
 import { PageOptions, SortOptions } from '@/app/types';
 
 export const appendPageOptionQuery = (
     api: string,
     options: PageOptions
 ): string => {
-    const query = {
-        pageNumber: options.number,
-        pageSize: options.size
-    };
+    const query: Param[] = [
+        {
+            key: 'pageNumber',
+            value: options.number.toString()
+        },
+        {
+            key: 'pageSize',
+            value: options.size.toString()
+        }
+    ];
 
     return appendQueryString(api, query);
 };
@@ -18,21 +22,71 @@ export const appendSortOptionQuery = (
     api: string,
     options: SortOptions
 ): string => {
-    const query = {
-        sortColumn: options.column,
-        sortDirection: options.direction
-    };
+    const query: Param[] = [
+        {
+            key: 'sortColumn',
+            value: options.column
+        },
+        {
+            key: 'sortDirection',
+            value: options.direction
+        }
+    ];
 
     return appendQueryString(api, query);
 };
 
-export const appendQueryString = (url: string, params: any): string => {
-    const parsed = queryString.parseUrl(url);
+export const appendQueryString = (url: string, params: Param[]): string => {
+    const parsed = parseUrl(url);
+    parsed.params = parsed.params.concat(params);
+    return formatUrl(parsed);
+};
 
-    const query = {
-        ...parsed.query,
-        ...params
+type Param = {
+    key: string;
+    value: string;
+};
+
+type Url = {
+    base: string;
+    params: Param[];
+};
+
+const parseUrl = (input: string): Url => {
+    var split = input.split('?');
+
+    const url: Url = {
+        base: split[0],
+        params: []
     };
 
-    return `${parsed.url}?${queryString.stringify(query)}`;
+    if (split.length === 1) return url;
+
+    const params = split[1].split('&');
+
+    params.forEach(p => {
+        const param = p.split('=');
+
+        if (param.length < 2) return;
+
+        url.params.push({
+            key: param[0],
+            value: param[1]
+        });
+    });
+
+    return url;
+};
+
+const formatUrl = (input: Url): string => {
+    let url = input.base;
+
+    if (input.params.length > 0) {
+        const query = input.params.map(p => {
+            return `${p.key}=${p.value}`;
+        });
+        url = `${url}?${query.join('&')}`;
+    }
+
+    return url;
 };
