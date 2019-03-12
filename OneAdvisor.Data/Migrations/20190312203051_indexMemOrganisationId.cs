b3dd3d7713350972261022b83e8f70369eba85be
Dean van Niekerk <deanvniekerk@gmail.com>
2019-03-12 22:42:49 +0200

    Added index for member organisationId

---
+++ OneAdvisor.Data/Migrations/20190312203051_indexMemOrganisationId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OneAdvisor.Data.Migrations
+{
+    public partial class indexMemOrganisationId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name = 'mem_member_OrganisationId_Index')
+BEGIN
+    CREATE NONCLUSTERED INDEX [mem_member_OrganisationId_Index] ON [dbo].[mem_Member] ([OrganisationId])
+END
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- OneAdvisor.Data/WorkingScripts/commission.sql
+++ OneAdvisor.Data/WorkingScripts/commission.sql
@@ -79,8 +79,6 @@ WITH
             m.LastName AS 'MemberLastName',
             m.Initials AS 'MemberInitials',
             m.DateOfBirth AS 'MemberDateOfBirth',
-            m.OrganisationId AS 'OrganisationId',
-            cs.Date AS 'Date',
             SUM(CASE WHEN (cs.DateMonth = 2 AND cs.DateYear = 2019 AND ct.CommissionEarningsTypeId = '8b42edc0-fac6-e946-c779-9d90a805c294')
     THEN c.AmountIncludingVAT ELSE 0 END) AS 'MonthlyAnnuityMonth',
             SUM(CASE WHEN ct.CommissionEarningsTypeId = 'e8799015-6f4a-5d45-5be9-0fcd516e0951'
@@ -97,6 +95,7 @@ WITH
             JOIN lkp_CommissionType ct ON c.CommissionTypeId = ct.id
             JOIN lkp_CommissionEarningsType cet ON ct.CommissionEarningsTypeId = cet.Id
         WHERE m.OrganisationId = '9a46c5ae-3f6f-494c-b0de-d908f08507c3'
+            AND cs.Date >= '2018-02-28' AND cs.Date <= '2019-02-28'
         GROUP BY m.Id, m.LastName, m.Initials, m.DateOfBirth
     ),
     CommissionQueryTotaled
@@ -122,12 +121,22 @@ FROM CommissionQueryNumbered
 WHERE RowNumber BETWEEN 1 AND 10
 
 
-CREATE NONCLUSTERED INDEX [MemberOrganisationId] ON [dbo].[mem_Member] ([CommissionStatementId]) INCLUDE ([AmountIncludingVAT], [CommissionTypeId], [PolicyId], [VAT]) WITH (ONLINE = ON)
+--CREATE NONCLUSTERED INDEX [memberOrganisationId] ON [dbo].[mem_Member] ([OrganisationId])
+--DROP INDEX [mem_member_OrganisationId_Index] ON [dbo].[mem_Member]
 
+IF NOT EXISTS(SELECT *
+FROM sys.indexes
+WHERE name = 'mem_member_OrganisationId_Index')
+    BEGIN
+    CREATE NONCLUSTERED INDEX [mem_member_OrganisationId_Index] ON [dbo].[mem_Member] ([OrganisationId])
+END
+
+SELECT *
+FROM sys.indexes
 
 
 SELECT
-    TOP 100
+    -- TOP 100
     m.Id AS 'MemberId',
     m.LastName AS 'MemberLastName',
     m.Initials AS 'MemberInitials',
@@ -142,6 +151,9 @@ FROM com_commission c
     JOIN lkp_CommissionEarningsType cet ON ct.CommissionEarningsTypeId = cet.Id
 WHERE m.OrganisationId = '9a46c5ae-3f6f-494c-b0de-d908f08507c3'
     --AND cs.Date >= '2018-02-28' AND cs.Date <= '2019-02-28' 
+
+
+
 
 
 
