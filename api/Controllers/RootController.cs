14174ac251ee4a01a5624355017c9bebf9abe329
Dean van Niekerk <deanvniekerk@gmail.com>
2019-03-18 15:13:56 +0200

    fix for emails

---
+++ api/Controllers/RootController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers.Member.Contacts
+{
+    [ApiController]
+    [Route("")]
+    public class RootController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return Ok(new { message = "Welcome to the OneAdvisor API." });
+        }
+    }
+}
