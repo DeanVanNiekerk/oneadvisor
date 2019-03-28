2613f8d2542c138ba819bfea5b27faa1ab852891
Dean van Niekerk <deanvniekerk@gmail.com>
2019-03-28 17:28:59 +0200

    remove const enum

--- web/src/state/app/directory/lookups/clientTypes/enums.ts
+++ web/src/state/app/directory/lookups/clientTypes/enums.ts
@@ -1,4 +1,4 @@
-export const enum ClientTypeId {
+export enum ClientTypeId {
     Individual = "27bb22b3-4c3d-41a3-48bf-690a98f8f780",
     Company = "295565bf-7485-85f1-6c98-947ab0b7770c",
     Trust = "55f6c0ef-ae2c-faac-adff-ea3bd269043f",
