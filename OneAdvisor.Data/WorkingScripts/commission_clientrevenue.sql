7ad57efb34d79b8d14a71ffa034c7653ccee5924
Dean van Niekerk <deanvniekerk@gmail.com>
2019-07-09 11:25:53 +0200

    updated client rev script

--- OneAdvisor.Data/WorkingScripts/commission_clientrevenue.sql
+++ OneAdvisor.Data/WorkingScripts/commission_clientrevenue.sql
@@ -12,6 +12,8 @@ WITH
     CommissionQuery
     AS
     (
+
+
                     SELECT
 
 
@@ -41,86 +43,69 @@ WITH
                 0 AS 'AllocationsCount'
 
             FROM clt_Client m
-                JOIN clt_Policy p ON m.Id = p.ClientId
-                JOIN com_commission c ON p.Id = c.PolicyId
-                JOIN com_CommissionStatement cs ON c.CommissionStatementId = cs.Id
+                JOIN clt_Policy p ON p.ClientId = m.Id
+                JOIN com_commission c ON c.PolicyId = p.Id
                 JOIN com_CommissionType ct ON c.CommissionTypeId = ct.id
-                JOIN com_CommissionEarningsType cet ON ct.CommissionEarningsTypeId = cet.Id
-
-
-                JOIN idn_User u ON p.UserId = u.Id
-
-
+                JOIN com_CommissionStatement cs ON c.CommissionStatementId = cs.Id
+                JOIN idn_User u ON c.UserId = u.Id
 
             WHERE m.OrganisationId = '9a46c5ae-3f6f-494c-b0de-d908f08507c3'
                 AND m.IsDeleted = 0
-
-
                 AND u.BranchId IN (@BranchId0)
-
                 AND cs.Date > @StartDate
-
                 AND cs.Date <= @EndDate
-
 
             GROUP BY m.Id, m.LastName, m.Initials, m.DateOfBirth
 
-        UNION
-
-            SELECT
-
-
-                m.Id AS 'ClientId',
-                m.LastName AS 'ClientLastName',
-                m.Initials AS 'ClientInitials',
-                m.DateOfBirth AS 'ClientDateOfBirth',
-
-                SUM(CASE WHEN 
-                    (cs.DateMonth = 4 AND cs.DateYear = 2019 AND ct.CommissionEarningsTypeId = '8b42edc0-fac6-e946-c779-9d90a805c294')
-                    THEN (c.AmountIncludingVAT - c.VAT) ELSE 0 END) AS 'MonthlyAnnuityMonth',
-
-                SUM(CASE WHEN 
-                    ct.CommissionEarningsTypeId = 'e8799015-6f4a-5d45-5be9-0fcd516e0951' 
-                    THEN (c.AmountIncludingVAT - c.VAT) ELSE 0 END) AS 'AnnualAnnuity',
-
-                SUM(CASE WHEN 
-                    ct.CommissionEarningsTypeId = '9f8fc29d-0f1c-b952-d446-79cc3ed967d7'
-                    THEN (c.AmountIncludingVAT - c.VAT) ELSE 0 END) AS 'OnceOff',
-
-                SUM(CASE WHEN 
-                    ct.CommissionEarningsTypeId = 'e7f98561-f018-3edd-2118-e3646c89e2a2'  
-                    THEN (c.AmountIncludingVAT - c.VAT) ELSE 0 END) AS 'LifeFirstYears',
-
-                SUM(c.AmountIncludingVAT - c.VAT) AS 'GrandTotal',
-
-                COUNT(*) AS 'AllocationsCount'
-
-            FROM clt_Client m
-                JOIN com_CommissionAllocation ca on m.Id = ca.ToClientId
-                JOIN com_CommissionAllocationPolicy cap on ca.Id = cap.CommissionAllocationId
-                JOIN clt_Policy p ON p.Id = cap.PolicyId
-                JOIN com_commission c ON p.Id = c.PolicyId
-                JOIN com_CommissionStatement cs ON c.CommissionStatementId = cs.Id
-                JOIN com_CommissionType ct ON c.CommissionTypeId = ct.id
-                JOIN com_CommissionEarningsType cet ON ct.CommissionEarningsTypeId = cet.Id
+        -- UNION ALL
+        --     SELECT
 
 
-                JOIN idn_User u ON p.UserId = u.Id
+        --         m.Id AS 'ClientId',
+        --         m.LastName AS 'ClientLastName',
+        --         m.Initials AS 'ClientInitials',
+        --         m.DateOfBirth AS 'ClientDateOfBirth',
+
+        --         SUM(CASE WHEN 
+        --             (cs.DateMonth = 4 AND cs.DateYear = 2019 AND ct.CommissionEarningsTypeId = '8b42edc0-fac6-e946-c779-9d90a805c294')
+        --             THEN (c.AmountIncludingVAT - c.VAT) ELSE 0 END) AS 'MonthlyAnnuityMonth',
+
+        --         SUM(CASE WHEN 
+        --             ct.CommissionEarningsTypeId = 'e8799015-6f4a-5d45-5be9-0fcd516e0951' 
+        --             THEN (c.AmountIncludingVAT - c.VAT) ELSE 0 END) AS 'AnnualAnnuity',
+
+        --         SUM(CASE WHEN 
+        --             ct.CommissionEarningsTypeId = '9f8fc29d-0f1c-b952-d446-79cc3ed967d7'
+        --             THEN (c.AmountIncludingVAT - c.VAT) ELSE 0 END) AS 'OnceOff',
+
+        --         SUM(CASE WHEN 
+        --             ct.CommissionEarningsTypeId = 'e7f98561-f018-3edd-2118-e3646c89e2a2'  
+        --             THEN (c.AmountIncludingVAT - c.VAT) ELSE 0 END) AS 'LifeFirstYears',
+
+        --         SUM(c.AmountIncludingVAT - c.VAT) AS 'GrandTotal',
+
+        --         COUNT(*) AS 'AllocationsCount'
+
+        --     FROM clt_Client m
+        --         JOIN com_CommissionAllocation ca on m.Id = ca.ToClientId
+        --         JOIN com_CommissionAllocationPolicy cap on ca.Id = cap.CommissionAllocationId
+        --         JOIN clt_Policy p ON p.Id = cap.PolicyId
+        --         JOIN com_commission c ON c.PolicyId = p.Id
+        --         JOIN com_CommissionType ct ON c.CommissionTypeId = ct.id
+        --         JOIN com_CommissionStatement cs ON c.CommissionStatementId = cs.Id
+        --         JOIN idn_User u ON c.UserId = u.Id
+
+        --     WHERE m.OrganisationId = '9a46c5ae-3f6f-494c-b0de-d908f08507c3'
+        --         AND m.IsDeleted = 0
+        --         --   AND u.BranchId IN (@BranchId0)
+        --         AND cs.Date > @StartDate
+        --         AND cs.Date <= @EndDate
+
+        --     GROUP BY m.Id, m.LastName, m.Initials, m.DateOfBirth
 
 
 
-            WHERE m.OrganisationId = '9a46c5ae-3f6f-494c-b0de-d908f08507c3'
-                AND m.IsDeleted = 0
 
-
-                AND u.BranchId IN (@BranchId0)
-
-                AND cs.Date > @StartDate
-
-                AND cs.Date <= @EndDate
-
-
-            GROUP BY m.Id, m.LastName, m.Initials, m.DateOfBirth
     ),
     CommissionQueryTotaled
     AS
