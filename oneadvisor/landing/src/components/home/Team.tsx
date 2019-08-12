1441ab388b28c47c5c70551ae1a65e12dd484354
Dean van Niekerk <deanvniekerk@gmail.com>
2019-08-12 07:41:54 +0200

    started on team section

--- oneadvisor/landing/src/components/home/Team.tsx
+++ oneadvisor/landing/src/components/home/Team.tsx
@@ -2,76 +2,38 @@ import React from "react";
 
 const Team: React.FC = () => {
     return (
-        <div>
-            top!
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-            sadfadf
-            <br />
-        </div>
+        <>
+            <div className="row mt-3">
+                <div className="col text-center">
+                    <h1 className="display-4">Team</h1>
+                </div>
+            </div>
+
+            <div className="row mt-3">
+                <div className="col-md-6">
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img className="card-img-top" />
+                        <div className="card-body">
+                            <p className="card-text">
+                                Some quick example text to build on the card title and make up the bulk of the card's
+                                content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img className="card-img-top" />
+                        <div className="card-body">
+                            <p className="card-text">
+                                Some quick example text to build on the card title and make up the bulk of the card's
+                                content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
