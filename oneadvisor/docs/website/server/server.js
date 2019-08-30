5455561e1fb9ad4f7767f0c61f28dd156234ea25
Dean van Niekerk <deanvniekerk@gmail.com>
2019-08-30 09:29:53 +0200

    added docs build

---
+++ oneadvisor/docs/website/server/server.js
@@ -0,0 +1,13 @@
+/*
+  Incredibly simple Node.js and Express application server for serving static assets.
+*/
+
+const express = require("express");
+const port = process.env.PORT || 8080;
+const app = express();
+
+// serve static assets
+app.use(express.static(__dirname + "./dist"));
+
+app.listen(port);
+console.log("server started on port " + port);
