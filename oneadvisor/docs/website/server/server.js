803a2d71313f0fa4a101f9fd2d78b400dd9ccb2c
Dean van Niekerk <deanvniekerk@gmail.com>
2019-08-30 09:31:11 +0200

    updated server

--- oneadvisor/docs/website/server/server.js
+++ oneadvisor/docs/website/server/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // serve static assets
-app.use(express.static(__dirname + "./dist"));
+app.use(express.static("dist"));
 
 app.listen(port);
 console.log("server started on port " + port);
