dbdb642636ffb94dec6d54c925e6b86e09d6d672
Dean van Niekerk <deanvniekerk@gmail.com>
2020-01-29 17:27:56 +0200

    fix failing test

--- test/OneAdvisor.Import.Excel.Test/Readers/CommissionImport/CommissionImportReaderTest.cs
+++ test/OneAdvisor.Import.Excel.Test/Readers/CommissionImport/CommissionImportReaderTest.cs
@@ -94,7 +94,7 @@ namespace OneAdvisor.Import.Excel.Test.Readers.CommissionImport
             Assert.Equal("code_2", actual.CommissionTypeCode);
             Assert.Equal("code_2", actual.CommissionTypeValue);
             Assert.Equal("ln_2", actual.LastName);
-            Assert.Null(actual.DateOfBirth);
+            Assert.Equal("", actual.DateOfBirth);
             Assert.Equal("fn_2", actual.FirstName);
             Assert.Equal("8210035032082", actual.IdNumber);
             Assert.Equal("ini_2", actual.Initials);
@@ -110,7 +110,7 @@ namespace OneAdvisor.Import.Excel.Test.Readers.CommissionImport
             Assert.Equal("ln_3", actual.LastName);
             Assert.Equal("1982-10-05", actual.DateOfBirth);
             Assert.Equal("fn_3", actual.FirstName);
-            Assert.Null(actual.IdNumber);
+            Assert.Equal("", actual.IdNumber);
             Assert.Equal("ini_3", actual.Initials);
             Assert.Equal("fullName_3", actual.FullName);
             Assert.Equal("brokerFullName_3", actual.BrokerFullName);
