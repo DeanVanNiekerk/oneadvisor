b914fcfc1a5a7d1ade462fc77ed9ebe7d410ac1d
Dean van Niekerk <deanvniekerk@gmail.com>
2020-04-03 11:30:42 +0200

    directory state SPLIT

--- src/oneadvisor/web/src/state/app/selectors.ts
+++ src/oneadvisor/web/src/state/app/selectors.ts
@@ -1,14 +1,15 @@
 import { createSelector } from "reselect";
 
-import { directoryLookupsSelector } from "../directory/lookups";
+//import { directoryLookupsSelector } from "../directory/lookups";
 import { RootState } from "../rootReducer";
 import { clientLookupsSelector } from "./client/lookups";
 import { commissionLookupsSelector } from "./commission/lookups";
 
 export const isLoadingLookupsSelector: (state: RootState) => boolean = createSelector(
-    directoryLookupsSelector,
+    //directoryLookupsSelector,
     commissionLookupsSelector,
     clientLookupsSelector,
-    (directoryLookups, commissionLookups, clientLookups) =>
-        directoryLookups.fetching || commissionLookups.fetching || clientLookups.fetching
+    //(directoryLookups, commissionLookups, clientLookups) =>
+    //directoryLookups.fetching || commissionLookups.fetching || clientLookups.fetching
+    (commissionLookups, clientLookups) => commissionLookups.fetching || clientLookups.fetching
 );
