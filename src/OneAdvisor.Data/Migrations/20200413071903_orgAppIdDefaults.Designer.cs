40d74f3418e962577c2b6534bd3ffcec15691194
Dean van Niekerk <deanvniekerk@gmail.com>
2020-04-13 09:25:42 +0200

    default appIds for organisations

--- src/OneAdvisor.Data/Migrations/20200413071903_orgAppIdDefaults.Designer.cs
+++ src/OneAdvisor.Data/Migrations/20200413071903_orgAppIdDefaults.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OneAdvisor.Data;
 
 namespace OneAdvisor.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200413071903_orgAppIdDefaults")]
+    partial class orgAppIdDefaults
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/OneAdvisor.Data/Migrations/20200413071903_orgAppIdDefaults.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OneAdvisor.Data.Migrations
+{
+    public partial class orgAppIdDefaults : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //Give all orgs access to Directory, Client and Commission
+            migrationBuilder.Sql("UPDATE dir_Organisation SET ApplicationIds = '[\"66c3b4e8-8a30-4a4b-be4d-3928d12fefe9\",\"605ea52c-3627-48e2-8f7c-4819c5ea555b\",\"2fca4500-9142-4940-aaf4-b18925c96d66\"]'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
