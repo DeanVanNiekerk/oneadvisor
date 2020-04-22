d8c5380e824476f9ce1188e5dd3247542e2076fc
Dean van Niekerk <deanvniekerk@gmail.com>
2020-04-22 20:55:57 +0200

    fixed test

--- test/oneadvisor/api.Test/Controllers/Client/MergeControllerTest.cs
+++ test/oneadvisor/api.Test/Controllers/Client/MergeControllerTest.cs
@@ -11,13 +11,15 @@ using OneAdvisor.Model.Client.Interface;
 using OneAdvisor.Model.Client.Model.Client;
 using OneAdvisor.Model.Client.Model.Client.Merge;
 using Xunit;
+using OneAdvisor.Model.Client.Model.Policy.Merge;
+using OneAdvisor.Model.Client.Model.Policy;
 
 namespace api.Test.Controllers.Client
 {
     public class MergeControllerTest
     {
         [Fact]
-        public void ClientEditModelComposition()
+        public void MergeClientsModelComposition()
         {
             Assert.Equal(2, typeof(MergeClients).PropertyCount());
             Assert.True(typeof(MergeClients).HasProperty("TargetClient"));
@@ -25,7 +27,15 @@ namespace api.Test.Controllers.Client
         }
 
         [Fact]
-        public async Task Merge()
+        public void MergePoliciesModelComposition()
+        {
+            Assert.Equal(2, typeof(MergePolicies).PropertyCount());
+            Assert.True(typeof(MergePolicies).HasProperty("TargetPolicy"));
+            Assert.True(typeof(MergePolicies).HasProperty("SourcePolicyIds"));
+        }
+
+        [Fact]
+        public async Task MergeClients()
         {
             var client = new ClientEdit()
             {
@@ -56,9 +66,54 @@ namespace api.Test.Controllers.Client
                 })
                 .ReturnsAsync(result);
 
-            var controller = new MergeController(service.Object, authService.Object);
+            var controller = new MergeController(service.Object, null, authService.Object);
 
-            var actual = await controller.Merge(merge);
+            var actual = await controller.MergeClients(merge);
+
+            Assert.Same(merge, merged);
+            Assert.Equal(Scope.Branch, options.Scope);
+
+            var okResult = Assert.IsType<OkObjectResult>(actual);
+            var returnValue = Assert.IsType<Result>(okResult.Value);
+
+            Assert.Same(result, returnValue);
+        }
+
+        [Fact]
+        public async Task MergePolicies()
+        {
+            var policy = new PolicyEdit()
+            {
+                Id = Guid.NewGuid(),
+            };
+            var merge = new MergePolicies()
+            {
+                TargetPolicy = policy,
+                SourcePolicyIds = new List<Guid>() { Guid.NewGuid(), Guid.NewGuid() }
+            };
+
+            var service = new Mock<IPolicyService>();
+            var authService = TestHelper.MockAuthenticationService(Scope.Branch);
+
+            var result = new Result()
+            {
+                Success = true
+            };
+
+            ScopeOptions options = null;
+            MergePolicies merged = null;
+
+            service.Setup(c => c.MergePolicies(It.IsAny<ScopeOptions>(), It.Is<MergePolicies>(m => m == merge)))
+                .Callback((ScopeOptions o, MergePolicies m) =>
+                {
+                    merged = m;
+                    options = o;
+                })
+                .ReturnsAsync(result);
+
+            var controller = new MergeController(null, service.Object, authService.Object);
+
+            var actual = await controller.MergePolicies(merge);
 
             Assert.Same(merge, merged);
             Assert.Equal(Scope.Branch, options.Scope);
