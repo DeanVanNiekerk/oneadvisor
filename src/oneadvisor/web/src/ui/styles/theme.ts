ca431b0881edd3b13432ae061043dfa8fd502867
Dean van Niekerk <deanvniekerk@gmail.com>
2020-04-25 20:48:18 +0200

    updated theme

--- src/oneadvisor/web/src/ui/styles/theme.ts
+++ src/oneadvisor/web/src/ui/styles/theme.ts
@@ -1,9 +1,39 @@
 import { OrdinalColorsInstruction } from "@nivo/colors";
 import { Theme } from "@nivo/core";
 
+// DARK THEME -----------------------------------------------------
+// //General
+// export const TEXT_COLOR = "rgba(255, 255, 255, 0.65)";
+// export const BACKGROUND_COLOR = "#1f1f1f";
+
+// //Charts
+// export const CHART_COLORS: OrdinalColorsInstruction = { scheme: "set3" };
+
+// export const CHART_THEME: Theme = {
+//     tooltip: {
+//         container: {
+//             backgroundColor: BACKGROUND_COLOR,
+//         },
+//     },
+//     grid: {
+//         line: {
+//             stroke: "rgba(255, 255, 255, 0.25)",
+//         },
+//     },
+//     axis: {
+//         ticks: {
+//             text: {
+//                 fill: TEXT_COLOR,
+//             },
+//         },
+//     },
+// };
+// --------------------------------------------------------------------
+
+// LIGHT THEME -----------------------------------------------------
 //General
-export const TEXT_COLOR = "rgba(255, 255, 255, 0.65)";
-export const BACKGROUND_COLOR = "#1f1f1f";
+export const TEXT_COLOR = "rgba(0, 0, 0, 0.65)";
+export const BACKGROUND_COLOR = "#F0F2F5";
 
 //Charts
 export const CHART_COLORS: OrdinalColorsInstruction = { scheme: "set3" };
@@ -16,7 +46,7 @@ export const CHART_THEME: Theme = {
     },
     grid: {
         line: {
-            stroke: "rgba(255, 255, 255, 0.25)",
+            stroke: "rgba(0, 0, 0, 0.25)",
         },
     },
     axis: {
@@ -27,3 +57,4 @@ export const CHART_THEME: Theme = {
         },
     },
 };
+// --------------------------------------------------------------------
